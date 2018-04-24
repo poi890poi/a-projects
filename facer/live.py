30a32fba6d6222674c140c132162c0a77e5f68f0
Lee.Lin <Lee.Lin@arobot.info>
2018-04-24 15:34:36 +0800

    Add video playback for face applications testing

--- facer/live.py
+++ facer/live.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import time
+from datetime import datetime
 import base64
 
 import json
@@ -8,6 +9,8 @@ from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 from urllib.error import URLError
 import urllib3
+
+import argparse
 
 from queue import Queue, Empty
 import threading
@@ -40,8 +43,8 @@ class ThreadUrl(threading.Thread):
                 postdata = json.dumps(task)
                 try:
                     #self.response = None
-                    url = 'http://192.168.41.41:9000/predict'
-                    #url = 'http://10.129.11.4:9000/predict'
+                    #url = 'http://192.168.41.41:9000/predict'
+                    url = 'http://10.129.11.4:9000/predict'
                     r = http.request('POST', url, body=postdata.encode())
                     response = json.loads(r.data.decode())
                     timing = response['timing']
@@ -60,129 +63,245 @@ class ThreadUrl(threading.Thread):
             #signals to queue job is done
             self.in_queue.task_done()
 
-# Initialize video capture object
-cap = cv2.VideoCapture(0)
+def time_string(input):
+    input = int(input)
+    milliseconds = str(input%1000).zfill(4)
+    seconds = (input/1000) % 60
+    seconds = str(int(seconds)).zfill(2)
+    minutes = (input/(1000*60)) % 60
+    minutes = str(int(minutes)).zfill(2)
+    hours = (input/(1000*60*60)) % 24
+    hours = str(int(hours)).zfill(2)
 
-interval = 1./25.
-t_ = time.time() + interval
-f_ = 0
-osd = []
+    return '{}:{}:{}.{}'.format(hours, minutes, seconds, milliseconds)
 
-# Start the thread for URL fetching
-for i in range(1):
-    t = ThreadUrl(in_queue, out_queue)
-    t.setDaemon(True)
-    t.start()
+def main(args):
+    name_table = {
+        'd747': 'Natalie Portman',
+        '05c1': 'Dana Carvey',
+        'd364': 'Emma Watson',
+        'e36e': 'Emma Watson',
+        '3283': 'Taylor Swift',
+        'ed29': "Conan O'Brien",
+        'c584': "Harrison Ford",
+        '62c6': "Graham Norton",
+        '0d26': "Reese Witherspoon",
+        '61fa': "Ryan Gosling",
+        'f98c': "no one",
+    }
+    tests = {
+        'cn01': {
+            'video': '../../data/youtube_clips/conan/d_carvey_01.mp4',
+        },
+        'cn02': {
+            'video': '../../data/youtube_clips/conan/j_daniels_01.mp4',
+        },
+        'cn03': {
+            'video': '../../data/youtube_clips/conan/j_manganiello_01.mp4',
+        },
+        'cn04': {
+            'video': '../../data/youtube_clips/conan/m_akerman_01.mp4',
+        },
+        'cn05': {
+            'video': '../../data/youtube_clips/conan/n_leggero_01.mp4',
+        },
+        'np01': {
+            'video': '../../data/youtube_clips/n_portman_01.mp4',
+        },
+        'ew01': {
+            'video': '../../data/youtube_clips/e_watson_01.mp4',
+        },
+        'ts01': {
+            'video': '../../data/youtube_clips/graham/t_swift_01.mp4',
+        },
+        'hf01': {
+            'video': '../../data/youtube_clips/graham/h_ford_01.mp4',
+        },
+    }
+    # Initialize video capture object
+    if args.vfile:
+        cap = cv2.VideoCapture(args.vfile)
+    elif args.test:
+        cap = cv2.VideoCapture(tests[args.test]['video'])
+        length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        print( length )
+    else:
+        cap = cv2.VideoCapture(0)
 
-EMOTIONS = ['angry', 'disgusted', 'fearful', 'happy', 'sad', 'surprised', 'neutral']
-font = cv2.FONT_HERSHEY_SIMPLEX
+    interval = 1./25.
+    t_ = time.time() + interval
+    f_ = 0
+    osd = []
 
-last_response = None
-mode = ''
-t_register_expiration = 0
+    # Start the thread for URL fetching
+    for i in range(1):
+        t = ThreadUrl(in_queue, out_queue)
+        t.setDaemon(True)
+        t.start()
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    if frame is None:
-        continue
-    t_frame = time.time() * 1000
+    EMOTIONS = ['angry', 'disgusted', 'fearful', 'happy', 'sad', 'surprised', 'neutral']
+    font = cv2.FONT_HERSHEY_SIMPLEX
 
-    # Our operations on the frame come here
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    last_response = None
+    mode = ''
+    t_register_expiration = 0
+    cd_play_ctrl = 0
+    is_paused = False
 
-    f_ += 1
-    if time.time() > t_:
-        # Do something every <interval> seconds
-
-        retval, bin_data = cv2.imencode('.jpg', frame)
-        requests = {
-            "requests": [
-                {
-                    "requestId": "3287de74a3c742ccc81e80eab881eaec",
-                    "media": {
-                        "content": base64.b64encode(bin_data).decode()
-                    },
-                    "services": [
-                        {
-                            "type": "face",
-                            #"model": "a-emoc", # Request emotion recognition
-                            "model": "fnet",
-                            "mode": mode,
-                            "options": {
-                                "res_cap": 448,
-                                "factor": 0.6,
-                                "interp": "NEAREST",
-                                "fnet-cooldown": 1500,
-                            }
-                        }
-                    ]
-                }
-            ],
-            'agent': {
-                'agentId': '192.168.41.41',
-                't_frame': t_frame,
-                'debug': 1,
-            },
-            'timing': {
-                'client_sent': time.time(),
-                't_expiration': time.time() + 0.25,
-            }
-        }
-
-        if in_queue.qsize() >= 1:
-            # in_queue is full
-            pass
+    while(True):
+        # Capture frame-by-frame
+        if not is_paused:
+            ret, frame = cap.read()
+            if not ret:
+                print('No more frame')
+                break
+            cached = np.array(frame)
         else:
-            in_queue.put(requests)
-            #print('qsize', in_queue.qsize())
+            frame = np.array(cached)
 
-        f_ = 0
-        t_ = time.time() + interval
+        if frame is None:
+            continue
+        t_frame = time.time() * 1000
 
-    try:
-        last_response = out_queue.get_nowait()
-        print('frame to get_nowait()', time.time()*1000 - last_response['agent']['t_frame'])
-    except Empty:
-        pass
+        # Our operations on the frame come here
+        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    if last_response:
-        #print(len(t.response['responses'][0]['services'][0]['results']['rectangles']))
-        for r in last_response['responses']:
-            for service in r['services']:
-                for i, r in enumerate(service['results']['rectangles']):
-                    cv2.rectangle(frame, (r[0], r[1]), (r[0]+r[2], r[1]+r[3]), (255, 0, 255))
-                    if 'emotions' in service['results'] and len(service['results']['emotions']):
-                        cv2.putText(frame, EMOTIONS[np.argmax(service['results']['emotions'][i])], (r[0], r[1]), font, 0.7, (255, 0, 255), 1, cv2.LINE_AA)
-                    if 'identities' in service['results']:
-                        identity = service['results']['identities']
-                        name = identity['name'][i][0:4]
-                        confidence = identity['confidence'][i]
-                        #confidence = service['results']['confidences'][i]
-                        if confidence > 0:
-                            tag = name + ' / ' + str(round(confidence, 2))
-                            cv2.putText(frame, tag, (r[0], r[1]), font, 0.7, (255, 0, 255), 1, cv2.LINE_AA)
+        f_ += 1
+        if time.time() > t_:
+            # Do something every <interval> seconds
 
-    osd = [str(time.time()*1000), repr(frame.shape)]
-    if mode=='register':
-        osd.append('REGISTERING NEW FACE...')
-    y_ = 30
-    for line in osd:
-        cv2.putText(frame, line, (10, y_), font, 1, (0, 255, 0), 1, cv2.LINE_AA)
-        y_ += 36
+            retval, bin_data = cv2.imencode('.jpg', frame)
+            requests = {
+                "requests": [
+                    {
+                        "requestId": "3287de74a3c742ccc81e80eab881eaec",
+                        "media": {
+                            "content": base64.b64encode(bin_data).decode()
+                        },
+                        "services": [
+                            {
+                                "type": "face",
+                                #"model": "a-emoc", # Request emotion recognition
+                                "model": "fnet",
+                                "mode": mode,
+                                "options": {
+                                    "res_cap": 448,
+                                    "factor": 0.6,
+                                    "interp": "NEAREST",
+                                    "fnet-cooldown": 1500,
+                                }
+                            }
+                        ]
+                    }
+                ],
+                'agent': {
+                    'agentId': '192.168.41.41',
+                    't_frame': t_frame,
+                    'debug': 1,
+                },
+                'timing': {
+                    'client_sent': time.time(),
+                    't_expiration': time.time() + 0.25,
+                }
+            }
 
-    if time.time() > t_register_expiration:
-        mode = ''
+            if in_queue.qsize() >= 1:
+                # in_queue is full
+                pass
+            else:
+                in_queue.put(requests)
+                #print('qsize', in_queue.qsize())
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    key = cv2.waitKey(1) & 0xFF
-    if key==ord('q'):
-        break
-    elif key==ord('r'):
-        mode = 'register'
-        t_register_expiration = time.time() + 0.5
+            f_ = 0
+            t_ = time.time() + interval
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+        try:
+            last_response = out_queue.get_nowait()
+            print('frame to get_nowait()', time.time()*1000 - last_response['agent']['t_frame'])
+        except Empty:
+            pass
+
+        if last_response:
+            #print(len(t.response['responses'][0]['services'][0]['results']['rectangles']))
+            for r in last_response['responses']:
+                for service in r['services']:
+                    for i, r in enumerate(service['results']['rectangles']):
+                        cv2.rectangle(frame, (r[0], r[1]), (r[0]+r[2], r[1]+r[3]), (255, 255, 0))
+                        if 'emotions' in service['results'] and len(service['results']['emotions']):
+                            cv2.putText(frame, EMOTIONS[np.argmax(service['results']['emotions'][i])], (r[0], r[1]), font, 0.7, (255, 0, 255), 1, cv2.LINE_AA)
+                        if 'identities' in service['results']:
+                            identity = service['results']['identities']
+                            name = identity['name'][i][0:4]
+                            if name in name_table: name = name_table[name]
+                            if not name: name = 'UNKNOWN'
+                            confidence = identity['confidence'][i]
+                            if confidence >= 0.3:
+                                confidence = str(round(confidence, 2))
+                                tag = '{} / {}'.format(name, confidence)
+                                cv2.putText(frame, tag, (r[0], r[1]), font, 0.7, (255, 255, 0), 1, cv2.LINE_AA)
+                            else:
+                                s_index = str(round(service['results']['sort_index'][str(i)], 2))
+                                tag = '{}'.format(s_index)
+                                cv2.putText(frame, tag, (r[0], r[1]), font, 0.7, (255, 255, 0), 1, cv2.LINE_AA)
+
+        pos = cap.get(cv2.CAP_PROP_POS_MSEC)
+        pos_str = time_string(pos)
+        osd = [pos_str, repr(frame.shape)]
+        if mode=='register':
+            osd.append('REGISTERING NEW FACE...')
+        if is_paused:
+            osd.append('PAUSED')
+        y_ = 30
+        for line in osd:
+            cv2.putText(frame, line, (10, y_), font, 1, (0, 255, 0), 1, cv2.LINE_AA)
+            y_ += 36
+
+        if time.time() > t_register_expiration:
+            mode = ''
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key==ord('q'):
+            print('QQQQQQQ')
+            break
+        elif key==ord('r'):
+            mode = 'register'
+            t_register_expiration = time.time() + 0.5
+        elif key==ord('.'): # Forward jump
+            for i in range(25*30):
+                cap.grab()
+        elif key==ord(','): # Back jump
+            pass
+        elif key==ord(' '): # Pause
+            if time.time() > cd_play_ctrl: # De-bounce of play control
+                cd_play_ctrl = time.time() + 0.25
+                if is_paused:
+                    is_paused = False
+                else:
+                    is_paused = True
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__== "__main__":
+    parser = argparse.ArgumentParser(description="""\
+        Feeding video frames to computer vision server""")
+    parser.add_argument(
+        '--vfile',
+        type=str,
+        default='',
+        help='Path to the media file.'
+    )
+    parser.add_argument(
+        '--test',
+        type=str,
+        default='',
+        help='Name of test preset.'
+    )
+    ARGS, unknown = parser.parse_known_args()
+    if unknown:
+        print(unknown)
+        raise Exception('Unknown argument')
+    main(ARGS)
